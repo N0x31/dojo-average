320f7c5736eb90c1744764b49ce9b5eb127854ee
Nono <lombardo.nora@gmail.com>
2017-09-29 11:01:42 +0200

    dojo réussi!

--- Average.java
+++ Average.java
@@ -4,9 +4,9 @@ class Average {
 		float total = 0;
 
 		// début de ton code ici !
-
-
-
+			for (int i = 0 ; i < values.length; i++){
+				total = values[i] + total;
+			}
 		// fin de ton code :)
 
 		return total / values.length;
